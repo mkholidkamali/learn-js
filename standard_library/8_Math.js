8acb758c6493dabb88ce9f0b532a78634b006f70
mkholidkamali <mohamadkholid.kamali@gmail.com>
2023-02-23 21:31:43 +0700

    Math

---
+++ standard_library/8_Math.js
@@ -0,0 +1,9 @@
+
+{
+    // Static & Method
+    console.log(Math.max(1,2,3,4,5,6));
+    console.log(Math.min(1,2,3,4,5,6));
+    console.log(Math.round(10.5));
+    console.log(Math.floor(10.4));
+    console.log(Math.ceil(10.6));
+}
